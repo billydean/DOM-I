6606ed8af64b3b8cac8fb8e8730f9802afa97326
billydean <billy.goehring@gmail.com>
2022-02-07 10:23:18 -0800

    finished all MVP tasks

--- src/index.js
+++ src/index.js
@@ -40,17 +40,17 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 /*
-  [ ] Give the class "italic" to all "header nav a" //navItems
+  [x] Give the class "italic" to all "header nav a" //navItems
   [ ] Add text content to all "header nav a"
-  [ ] Add src to #logo-img
-  [ ] Add text to ".cta-text h1" and ".cta-text button"
-  [ ] Add src to #cta-img
-  [ ] Add text to all <h4> and <p> in .text-content in .top-content
-  [ ] Add src to #middle-img
-  [ ] Add text to all <h4> and <p> in .text-content in .bottom-content
-  [ ] Add <h4> and <p> text to .contact
-  [ ] Add class .bold to "footer a"
-  [ ] Add text to "footer a"
+  [x] Add src to #logo-img // logo
+  [x] Add text to ".cta-text h1" and ".cta-text button" // ctaText and ctaButton
+  [x] Add src to #cta-img // cta
+  [x] Add text to all <h4> and <p> in .text-content in .top-content // topHeaders and topText
+  [x] Add src to #middle-img // middleImg
+  [x] Add text to all <h4> and <p> in .text-content in .bottom-content // bottomHeaders and bottomText
+  [x] Add <h4> and <p> text to .contact // contactHeader and contactText
+  [x] Add class .bold to "footer a" // footer
+  [x] Add text to "footer a"
 */
 
 const navItems = document.querySelectorAll("header nav a");
@@ -65,6 +65,33 @@ const bottomText = document.querySelectorAll(".bottom-content .text-content p");
 const middleImg = document.querySelector("#middle-img");
 const contactHeader = document.querySelector(".contact h4");
 const contactText = document.querySelectorAll(".contact p");
-const footer = document.querySelecter("footer a");
+const footer = document.querySelector("footer a");
 
-console.log(navItems);
+navItems.forEach(item => item.className = "italic");
+footer.className = "bold";
+logo.src = "http://localhost:9000/img/logo.png";
+cta.src = "http://localhost:9000/img/cta.png";
+middleImg.src = "http://localhost:9000/img/accent.png";
+ctaText.textContent = "DOM Is Awesome";
+ctaButton.textContent = "Get Started";
+contactHeader.textContent = "Contact";
+footer.textContent = "Copyright Great Idea! 2021";
+topHeaders[0].textContent = "Features";
+topHeaders[1].textContent = "About";
+bottomHeaders[0].textContent = "Services";
+bottomHeaders[1].textContent = "Product";
+bottomHeaders[2].textContent = "Vision";
+topText[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+topText[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bottomText[0].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bottomText[1].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bottomText[2].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+contactText[0].textContent = "123 Way 456 Street Somewhere, USA";
+contactText[1].textContent = "1 (888) 888-8888";
+contactText[2].textContent = "sales@greatidea.io";
+navItems[0].textContent = "Services";
+navItems[1].textContent = "Product";
+navItems[2].textContent = "Vision";
+navItems[3].textContent = "Features";
+navItems[4].textContent = "About";
+navItems[5].textContent = "Contact";
